7c1f6ce029ab7b8c9a3031a4542ad67e4411d13f
Fernando <fernandorporto@outlook.com>
2021-11-19 23:22:26 -0300

    acrescentada msg como vai

--- projeto teste Git/Program.cs
+++ projeto teste Git/Program.cs
@@ -5,6 +5,7 @@ namespace projeto_teste_Git {
         static void Main(string[] args) {
             Console.WriteLine("Teste de Repositório Git");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("Como você está???");
 
             Console.ReadLine();
         }
