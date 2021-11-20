79ecf9ca5ebcc15d4eca542bd213281312f820e5
Fernando <fernandorporto@outlook.com>
2021-11-19 22:40:07 -0300

    teste inicial ola mundo

---
+++ projeto teste Git/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace projeto_teste_Git {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Teste de Repositório Git");
+            Console.ReadLine();
+        }
+    }
+}
