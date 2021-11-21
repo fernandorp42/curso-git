35d79e9c8b4decc7f07a2f2092d28642e8618663
Fernando <fernandorporto@outlook.com>
2021-11-20 21:09:36 -0300

    insercao variavel n

--- projeto teste Git/Program.cs
+++ projeto teste Git/Program.cs
@@ -6,8 +6,10 @@ namespace projeto_teste_Git {
             Console.WriteLine("Teste de Repositório Git");
             Console.WriteLine("Bom dia");
             Console.WriteLine("Como você está???");
-
+            int n = 0;
+            n = n + 1;
             Console.ReadLine();
+            Console.WriteLine(n);
         }
     }
 }
