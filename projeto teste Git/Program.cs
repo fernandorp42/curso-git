61046f1a34ff62cd0609b69f676b7755c943d2ce
Fernando <fernandorporto@outlook.com>
2021-11-22 22:58:14 -0300

    apagada linha 2 acres linha 3

--- projeto teste Git/Program.cs
+++ projeto teste Git/Program.cs
@@ -4,8 +4,8 @@ namespace projeto_teste_Git {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Teste de Repositório Git");
-            Console.WriteLine("Bom dia");
             Console.WriteLine("Como você está???");
+            Console.WriteLine("Teste apagar linha e inserir");
             int n = 0;
             n = n + 1;
             Console.ReadLine();
